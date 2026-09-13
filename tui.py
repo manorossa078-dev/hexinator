f1c424c8d3523cec9f5d43bfe6d72786c3a47cea
manorossa078-dev <manorossa078@gmail.com>
2026-09-13 13:53:52 +0200

    Update tui.py

--- tui.py
+++ tui.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-_D='Error. Perhaps you forgot to specify an output path?'
-_C='Contact me to gain Full-Access (FA) to use this program without waiting.'
+_H='Error. Is your input a valid text? Or it your output_input path?'
+_G='cipher.sh'
+_F='Error. Perhaps you forgot to specify an output path?'
+_E='decipher.sh'
+_D='Contact me to gain Full-Access (FA) to use this program without waiting.'
+_C='bin'
 _B=None
 _A='store_true'
-VER='hexinator v.0.0.3 LA'
-import argparse,subprocess,time
+VER='hexinator v.0.0.4 LA'
+import argparse,subprocess,time,os
+BASE=os.path.dirname(os.path.abspath(__file__))
 parser=argparse.ArgumentParser(description="This is a tool for all the lazy or workaholic Linux users. It makes you cipher and decipher hex-encoded lines with ease in a very lightweight tool. You're currently on LA (Limited Access), so you'll have a delay of 10 seconds.")
 parser.add_argument('input',help='The input field for your hex/text',nargs='?')
 parser.add_argument('output_input',help='The output input field is used only if you choose to write the results to a file',nargs='?')
@@ -16,14 +21,22 @@ args=parser.parse_args()
 try:
 	if args.version:print(VER)
 	elif args.hex and args.input is not _B:
-		print(_C);time.sleep(10)
+		print(_D);time.sleep(10)
 		if args.output and args.output_input is not _B:
-			with open(args.output+'.txt','w')as f:subprocess.run(['./bin/decipher.sh',args.input],stdout=f)
-		else:print(_D)
+			with open(args.output_input+'.txt','w')as f:res=subprocess.run([os.path.join(BASE,_C,_E),args.output_input],stdout=f)
+			if res.returncode!=0:print('Error. Is your output_input a valid hex?');exit(res.returncode)
+		elif args.output:print(_F)
+		else:
+			result=subprocess.run([os.path.join(BASE,_C,_E),args.input])
+			if result.returncode!=0:print('Error. Is your input a valid hex? Or it your output_input path?');exit(result.returncode)
 	elif args.text and args.input is not _B:
-		print(_C);time.sleep(10)
+		print(_D);time.sleep(10)
 		if args.output and args.output_input is not _B:
-			with open(args.output+'.txt','w')as f:subprocess.run(['./bin/cipher.sh',args.input],stdout=f)
-		else:print(_D)
-	else:print("There was an error... Try filling the input argument if you're not using -h or -v")
-except KeyboardInterrupt:print('\nQuitting hexinator...\nThanks you for using my tool.');exit(0)
+			with open(args.output_input+'.txt','w')as f:res=subprocess.run([os.path.join(BASE,_C,_G),args.output_input],stdout=f)
+			if res.returncode!=0:print(_H);exit(res.returncode)
+		elif args.output:print(_F)
+		else:
+			result=subprocess.run([os.path.join(BASE,_C,_G),args.input])
+			if result.returncode!=0:print(_H);exit(result.returncode)
+	else:print("There was an error. Try filling the input argument if you're not using -h or -v")
+except KeyboardInterrupt:print('\nQuitting hexinator...\nThank you for using my tool.');exit(0)
